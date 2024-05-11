4120c66a62c85da4115001d45b4e4dc832251ebc
Samuel Dippert <samueldippert04@gmail.com>
2024-05-11 15:00:46 +0200

    Update scriptglobe.js

--- js/scriptglobe.js
+++
@@ -1,8 +0,0 @@
-function closePopUp() {
-    var x = document.getElementsByClassName("we-pp");
-    if (x.style.opacity === "1") {
-      x.style.opactiy = "0";
-    } else {
-      x.style.opacity = "1";
-    }
-  }
